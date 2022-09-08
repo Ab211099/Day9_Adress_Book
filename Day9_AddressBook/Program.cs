1825513d26325c6c1b608d24126afee91da46a16
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-08 15:13:43 +0530

    [Abhi]Added:Contact in address book

--- Day9_AddressBook/Program.cs
+++ Day9_AddressBook/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Day9_AddressBook
+﻿
+
+namespace Day9_AddressBook
 {
+   
     internal class Program
-    {
-        static void Main(string[] args)
+   {
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Welcome to addres book");
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            Contact addressBook = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.displayContact();
+            Console.ReadLine();
         }
+        
+
     }
+    
+   
 }
